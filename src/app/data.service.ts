5ede9a5f0eff305f1ad5dcf5e84eddd5506f9691
Cristinel <cristinel@projects-abroad.net>
2018-04-18 10:47:14 +0300

    Fix bucket list count

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,7 +4,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject'
 @Injectable()
 export class DataService {
 
-    private goals = new BehaviorSubject<any>(['The initial goal', 'Another silly life goal']);
+    private goals = new BehaviorSubject<any>(['Learn AngularJs', 'Work with REST APIs', 'Learn React']);
     goal = this.goals.asObservable();
 
     constructor() { }
